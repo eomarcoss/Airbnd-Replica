ad3e36a85246d3aa587e087875d6bc204c3e5986
eomarcoss <marcoscesarmarinhocunha@gmail.com>
2026-04-10 11:17:04 -0300

    parte 12

--- front/src/components/Item.jsx
+++ front/src/components/Item.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Item = ({ place }) => {
   return (
-    <a href="/" className="flex flex-col gap-3">
+    <Link to={`/place/${place._id}`} className="flex flex-col gap-3">
       <img
         src={place.photos[0]}
         alt=""
@@ -17,7 +18,7 @@ const Item = ({ place }) => {
       <p>
         <span className="font-semibold">R$ {place.price}</span> por noite
       </p>
-    </a>
+    </Link>
   );
 };
 
